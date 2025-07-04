a07758e9a0b57032a004acf47b9f092080b23717
JOSE NU�EZ VILLALOBOS <jose.nunez.villalobos@est.una.ac.cr>
2025-07-04 12:10:39 -0600

    responsive y botones corregidos

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,30 +1,27 @@
+import Boton from "../../components/Boton";
 import { getEntidad } from "../../data";
 import type { Hero } from "../../models/hero";
 
+const HeroSection = () => {
+  const hero: Hero = getEntidad("hero");
 
-const HeroSection =  () => {
+  const rutas = ["/ver-mas", "/contactanos"];
 
-    const hero: Hero = getEntidad("hero");
-
-    return (
-   <section className="min-h-[700px] flex flex-col justify-end items-start bg-night text-basicWhite">
-    <div className="flex  flex-col gap-y-8">
-        <h2 className="text-6xl font-poppins font-bold">{hero.titulo}</h2>
-        <p className="font-opensans mt-2 items-start">{hero.descripcion}</p>
-        <div className="mt-4 flex justify-center gap-10">
-            {hero.botones.map((texto, idx) => (
-                <button
-                    key={idx}
-                    className="bg-amaranthPink font-poppins text-basicWhite px-4 py-2 rounded-2xl w-36">
-                    {texto}
-                </button>
-    ))} 
+  return (
+    <section className="min-h-[700px] flex flex-col justify-end items-start bg-night text-basicWhite px-10 sm:px-8 md:px-25 ">
+      <div className="flex flex-col gap-y-8">
+        <h2 className="text-4xl sm:text-5xl md:text-6xl font-poppins font-bold">{hero.titulo}</h2>
+        <p className="font-poppins text-sm sm:text-base md:text-lg">{hero.descripcion}</p>
+        <div className="mt-4 flex justify-center gap-4 sm:gap-1 lg:gap-10">
+          {hero.botones.map((texto, idx) => (
+            <Boton key={idx} where={rutas[idx]}>
+              {texto}
+            </Boton>
+          ))}
         </div>
-    </div>
-</section>
+      </div>
+    </section>
+  );
+};
 
-    )
-
-}
-
-export default HeroSection
+export default HeroSection;
