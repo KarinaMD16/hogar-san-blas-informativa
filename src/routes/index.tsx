681452621711ca3025fc3482cbedc524a1e2c15a
elein06 <rodriguezrayeli06@gmail.com>
2025-07-04 12:38:28 -0600

    resolución de conflictos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import Servicios from '../sections/servicios/Servicios'
 import Unete from '../sections/unirse/Unete'
-<<<<<<< HEAD
 import Historia from '../sections/historia/Historia'
-=======
 import Hero from '../sections/hero/Hero'
->>>>>>> a07758e9a0b57032a004acf47b9f092080b23717
 
 export const Route = createFileRoute('/')({
   component: Index,
