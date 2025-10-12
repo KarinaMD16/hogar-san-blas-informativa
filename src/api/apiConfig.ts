334c91e93e190ff1cfb1e3a2cd7681137704c19b
heyitskarinamd@outlook.com <heyitskarinamd@outlook.com>
2025-10-12 17:43:32 -0600

    ,

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const sidnamAPI = axios.create({
-  baseURL: 'https://sidnam-backend-2.onrender.com/',
+  baseURL: 'https://sidnam-backend-2.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 })
 
