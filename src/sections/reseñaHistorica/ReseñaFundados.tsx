f07a82a57408b2978113428605ddaa55325ac678
elein06 <rodriguezrayeli06@gmail.com>
2026-03-08 14:10:51 -0600

    correcciones pagina de reseña

--- src/sections/reseñaHistorica/ReseñaFundados.tsx
+++ src/sections/reseñaHistorica/ReseñaFundados.tsx
@@ -10,10 +10,13 @@ const ReseñaFundados = () => {
         style={{
             backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${contentJson.paginaReseñaHistorica[0].imagen})`
         }}
-        className="bg-cover bg-center bg-no-repeat min-h-[400px] sm:min-h-[400px] md:min-h-[500px] lg:min-h-[700px] h-[35vh] sm:h-[40vh] md:h-[60vh] lg:h-[70vh] text-white"
+        className="bg-cover bg-center bg-no-repeat  min-h-[720px] lg:min-h-160 
+        flex flex-col justify-end items-start 
+        bg-night text-basicWhite 
+        sm:px-8 md:px-25"
     >
         
-        <div className="absolute flex flex-col items-baseline w-9/10 sm:w-4/5 text-left gap-3 sm:gap-14 top-24 sm:top-40 left-4 sm:left-5 px-0">
+        <div className="absolute flex flex-col items-baseline w-9/10 sm:w-4/5 text-left gap-3 sm:gap-14 top-24 sm:top-34 left-4 sm:left-5 px-25">
             <h1 className="font-poppins text-white text-2xl sm:text-3xl md:text-6xl font-bold text-shadow-md leading-tight tracking-normal">
                 {reseña.titulo}
             </h1>
