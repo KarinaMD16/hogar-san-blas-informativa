ffc902a7bc13c921de6818b8382900b5eeefaf87
elein06 <rodriguezrayeli06@gmail.com>
2026-03-18 20:43:50 -0600

    scrollToTop routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,10 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { ScrollToTop } from '../components/ScrollToTop'
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <ScrollToTop />
       <Outlet />
     </>
   ),
