98e82daa8b32382ab7aed0264bb61b86b160eecf
heyitskarinamd@outlook.com <heyitskarinamd@outlook.com>
2026-04-20 14:51:40 -0600

    animaciones y responsive

--- src/components/CardDonacion.tsx
+++ src/components/CardDonacion.tsx
@@ -10,27 +10,28 @@ const CardDonacion = ({ publicacion }: PublicacionProps) => {
     <div
       className="bg-basicWhite p-3 sm:p-4 rounded-lg shadow-md hover:shadow-lg
       transition-shadow duration-300
-      w-full max-w-lg sm:w-60 h-auto lg:w-80 md:w-60
+      w-[84vw] max-w-88 sm:w-60 h-auto lg:w-80 md:w-60
+      flex-none snap-start
       flex flex-col items-center justify-between gap-2
     "
     >
 
       <div className="flex flex-col gap-2 w-full">
         <img
-          className="rounded-md h-32 sm:h-40 object-cover w-full"
+          className="rounded-md h-36 sm:h-40 object-cover w-full"
           src={publicacion.imagenUrl}
           alt={publicacion.Titulo}
           width={320}
           height={160}
         />
         <div className="flex items-start">
-          <p className="text-xs sm:text-sm truncate">{publicacion.fecha}</p>
+          <p className="text-xs sm:text-sm wrap-break-word">{publicacion.fecha}</p>
         </div>
       </div>
 
       <div className="w-full px-1">
-        <h2 className="mb-2 text-left font-poppins font-bold text-sm sm:text-lg line-clamp-3">{publicacion.Titulo}</h2>
-        <p className="text-left text-xs sm:text-sm line-clamp-3">{publicacion.Descripcion}</p>
+        <h2 className="mb-2 text-left font-poppins font-bold text-sm sm:text-lg line-clamp-3 wrap-break-word">{publicacion.Titulo}</h2>
+        <p className="text-left text-xs sm:text-sm line-clamp-3 wrap-break-word">{publicacion.Descripcion}</p>
       </div>
 
       <a href="/formularios/donacion#formularioDonacion"
